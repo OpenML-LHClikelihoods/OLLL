3a1a5d08f83c5dfa52cfccde675cdb0c8742de6d
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2026-09-10 13:24:12 +0200

    test

--- test.py
+++ test.py
@@ -2,17 +2,18 @@
 
 from nnAdapter import NNAdapter
 regions = [ 'SR1cut_cuts-0', 'SR2cut_cuts-0' ]
-onnxFile = "test.onnx"                                                          
-                                                                                
-adapter = NNAdapter ( onnxFile, session_options = {} )                          
-                                                                                
-yields = {}
-bkg_yields = adapter.onnxMeta["bkg_yields"]
-for region in regions: # predict for no yields                                  
-    yields[ region ] = bkg_yields[region]
-ret = adapter.predict ( yields )                                                
+onnxFile = "test.onnx"
 
-for srname,smyield in bkg_yields.items():
+adapter = NNAdapter ( onnxFile, session_options = {} )
+
+yields = {}
+yields = adapter.onnxMeta["bkg_yields"]
+for srname, bkg_yield in yields.items():
+    ## add a signal here
+    yields[srname] += 0.
+ret = adapter.predict ( yields )
+
+for srname,smyield in yields.items():
     print ( srname, smyield )
-   
-print ( "\n".join( f"{key:10s}: {value:.1f}" for key,value in ret.items()) ) 
+
+print ( "\n".join( f"{key:10s}: {value:.1f}" for key,value in ret.items()) )
