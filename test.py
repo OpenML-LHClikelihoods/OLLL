916d9e33d069612c0aa2bfba3855f190ef5ef745
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2026-09-10 13:20:31 +0200

    bkgyields

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
 from nnAdapter import NNAdapter
-regions = [ 'SR1cut_cuts', 'SR2cut_cuts' ]
+regions = [ 'SR1cut_cuts-0', 'SR2cut_cuts-0' ]
 onnxFile = "test.onnx"                                                          
                                                                                 
 adapter = NNAdapter ( onnxFile, session_options = {} )                          
                                                                                 
-yields = {}                                                                     
+yields = {}
+bkg_yields = adapter.onnxMeta["bkg_yields"]
 for region in regions: # predict for no yields                                  
-    yields[ region ] = 0.                                                       
+    yields[ region ] = bkg_yields[region]
 ret = adapter.predict ( yields )                                                
+
+for srname,smyield in bkg_yields.items():
+    print ( srname, smyield )
+   
 print ( "\n".join( f"{key:10s}: {value:.1f}" for key,value in ret.items()) ) 
